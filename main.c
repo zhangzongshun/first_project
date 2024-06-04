535ea7994f176bd1888e77cfe1d07ae49b477ad3
zhangzongshun <zhangzongshunde@outlook.com>
2024-06-04 19:37:18 +0800

    小改动
    添加了整数加法函数

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int add(int x, int y);
+
 int main()
 {
   printf("hello github!");
+  printf("x+y = %d\n", x+y);
   return 0;
+}
+
+int add(int x, int y)
+{
+  return x+y;
 }
